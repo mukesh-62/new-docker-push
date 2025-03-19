72f74cbe83d6432f4bb42a37b34bb3eda79eee74
MUKESH S <mukeshs.22it@kongu.edu>
2025-03-19 15:48:01 +0530

    Delete New_docker_push directory

--- New_docker_push/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Variables
-IMAGE_NAME="mukesh-62/new-docker-push"
-TAG="latest"
-
-# Build Docker image
-docker build -t $IMAGE_NAME:$TAG .
-echo "Docker image $IMAGE_NAME:$TAG built successfully."
